fc8e80dc06c3a1b5a770d22dbf71a1d44b45af5c
Diogo Amaral <di090.amaral@gmail.com>
2019-09-10 19:45:54 -0300

    Example config files

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'edd0e1797c404223e3b2eb7b114ca2e7',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '',
+  username: '',
+  password: '',
+  database: '',
+  port: 5432,
+  define: {
+    timestamp: true,
+    underscored: true,
+    undescoredAll: true,
+  },
+};
